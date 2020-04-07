c04c1d76b0b01dd92fc4a83cf3167f5a75c09a46
I516430 <shivesh.navin@sap.com>
2020-04-07 12:49:03 +0530

    update code

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+const bodyParser = require("body-parser");
 
 app.use(express.static(path.join(__dirname, './')));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 app.all('/', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
@@ -10,16 +16,27 @@ app.all('/', function (req, res) {
 
 
 app.all('/view', function (req, res) {
+	var cd =x;
+	if(req.query.cd!==undefined)
+		cd = x2;
+
 	if(req.body == undefined || req.body.code == undefined)
 	{
 		req.body={
-			code:'[code]'
+			code:cd
 		}
 	}
-	var fs=require('fs');
-	var html=""+fs.readFileSync('./view.html');
-	html.replace('[{{code}}]',req.body.code);
-	res.send(html);
+	var fs=require('fs'); 
+	fs.readFile('./view.html', 'utf8', function (err,data) {
+	  if (err) {
+		return console.log(err);
+	  }
+	  var result = data.replace(/----code----/g, req.body.code);
+	   res.send(result);
+	
+	});
+
+
 })
 
 
@@ -27,3 +44,8 @@ app.listen(process.env.PORT || 4000, function () {
     console.log("Server Started At ", process.env.PORT || 4000)
 
 })
+
+var x=`[a]->[b]`;
+
+var x2=`[a]->[b]
+[b]-->[c]`;
